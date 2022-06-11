cc61a5e25bd67694b9693cb280959ec28a48438b
Leticia <ln9458143@gmail.com>
2022-06-11 18:47:48 -0300

    Login e Cadastro responsivo

--- site/src/pages/cadastro/index.js
+++ site/src/pages/cadastro/index.js
@@ -8,6 +8,8 @@ export default function Cad() {
     <div className='corzinha2'>
 
 <div className='content'>
+<div className='loginsub'>
+
    
    <main className="login">
 
@@ -16,22 +18,29 @@ export default function Cad() {
 
    <p> 
             <label class="lab">Nome de usuário</label>
-            <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="Crie um nome de usúario" />
+            <input id="nome_cad" name="nome_cad" 
+            className='email_login2'
+            required="required" type="text" placeholder="Crie um nome de usúario" />
           </p>
 
              <p> 
               <label class="lab"> Email </label>
-               <input id="email_login" name="email_login" required="required" type="email" placeholder="Insira o seu e-mail :)"/>
+               <input id="email_login" name="email_login" required="required" type="email" 
+               className='email_login2'
+               placeholder="Insira o seu e-mail :)"/>
              </p>
              <p> 
                <label class="lab">Crie sua senha</label>
-               <input id="senha_login" name="senha-login" required="required" type="password" placeholder="Insira a sua senha :)" /> 
+               <input id="senha_login" name="senha-login" required="required" type="password" 
+               className='email_login2'
+               
+               placeholder="Insira a sua senha :)" /> 
              </p>
              <p> 
-               <input type="submit" value="Cadastrar"  /> 
+               <button>Cadastrar</button> 
              </p>
 
-              <div className='subb-1' >
+              <div className='subb1' >
 
               <p className='desc-txt'>Já tem conta?</p>
               <Link to='../login'>
@@ -39,10 +48,10 @@ export default function Cad() {
               </Link>
 
               </div>
-
+              <img src='./images/Humaaans - Ui Sketch.png' className='img1'></img>
    </main>
    </div>
-   <img src='./images/Humaaans - Ui Sketch.png' className='img1'></img>
+   </div>
         
     </div>
     )
--- site/src/pages/login/index.js
+++ site/src/pages/login/index.js
@@ -42,17 +42,22 @@ export default function Login() {
 
           <div className='content'>
    
-        <main className="login">
+      <div className='loginsub'>
 
+        <main className="login">
 
         <h1 className='tit-login'>Faça login</h1> 
                   <p> 
                    <label class="lab"> Email </label>
-                    <input id="email_login" name="email_login" required="required" type="email" placeholder="Insira o seu e-mail :)" value={email} onChange={ e => setEmail(e.target.value) }/>
+                    <input id="email_login" name="email_login" 
+                    className='email_login1'
+                    required="required" type="email" placeholder="Insira o seu e-mail :)" value={email} onChange={ e => setEmail(e.target.value) }/>
                   </p>
                   <p> 
                     <label class="lab">Senha</label>
-                    <input id="senha_login" name="senha-login" required="required" type="password" placeholder="Insira a sua senha :)"  value={senha} onChange={ e => setSenha(e.target.value) }/> 
+                    <input id="senha_login" name="senha-login" 
+                    className='email_login1'
+                    required="required" type="password" placeholder="Insira a sua senha :)"  value={senha} onChange={ e => setSenha(e.target.value) }/> 
                   </p>
                   <p> 
                     <button onClick={entrarClick}>Logar</button>
@@ -61,24 +66,28 @@ export default function Login() {
                   <div className='saa'>
                     {erro}
                   </div>
-                  
-                  <div className='subb-1'>
-                  <p className='desc-txt'>
+                  <div className='ab'>
+                   <p className='desc-txt'>
                     Ainda não criou a sua conta?
+                  </p>
+
                     <Link to='../cadastro' >
-                    <a className='mod'>
+                    <a href='' className='mod'>
                     Cadastre-se
                     </a> 
                     </Link>
+                  
                     
-                  </p>
                   </div>
+                  <div className='subb-1'>
+                  
+                    <img src='./images/Humaaans - Ui Sketch.png' className='img1'></img>
+                    </div>
                
-
-
         </main>
         </div>
-        <img src='./images/Humaaans - Ui Sketch.png' className='img1'></img>
+        </div>
+      
     </div>
     )
 }
